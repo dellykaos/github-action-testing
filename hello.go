8ff1b1060789fd38c964d6802b363f962c01cb73
delly nyman <dellykaos@gmail.com>
2020-06-06 20:49:17 +0700

    test

--- hello.go
+++ hello.go
@@ -1,6 +1,16 @@
 package hello
 
+import "fmt"
+
 //Add add a with b value
 func Add(a, b int) int {
 	return a + b
+}
+
+//Name will show 'Hello, #{name}'
+func Name(n string) string {
+	if len(n) > 0 {
+		return fmt.Sprintf("Hello, %s!", n)
+	}
+	return "Hello, world!"
 }
